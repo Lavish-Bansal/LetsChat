742935b843f1432fecaaa3ec42117ca5cc7ee26f
Lavish Bansal <lavishbansal1711@gmail.com>
2021-11-05 17:15:22 +0530

    deployed the server

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://intense-woodland-10014.herokuapp.com/');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
